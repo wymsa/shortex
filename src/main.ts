d657abd1297d39b183cd4f8489222eab67e60ab9
Anton Poliaiev <wymsa.email@gmail.com>
2025-07-08 15:21:30 +0300

    chore: nestjs setup

---
+++ src/main.ts
@@ -0,0 +1,14 @@
+import { AppModule } from '@/app.module';
+import { ConfigService } from '@nestjs/config';
+import { NestFactory } from '@nestjs/core';
+
+async function bootstrap() {
+	const app = await NestFactory.create(AppModule);
+	const configService = app.get<ConfigService>(ConfigService);
+
+	const PORT = configService.getOrThrow<number>('APP_PORT');
+
+	await app.listen(PORT);
+}
+
+bootstrap();
